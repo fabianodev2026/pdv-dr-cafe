2ea41449178f3f2b719fdc3446f4d715a6ff3868
fabianodev2026 <fabiano.ferreira.dev@gmail.com>
2026-08-22 10:13:44 -0300

    Initialize updater and process plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -98,6 +98,8 @@ try {{
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![
             open_cash_drawer,
             print_receipt,
